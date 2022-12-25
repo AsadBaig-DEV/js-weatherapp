b20ddda7c9142919761220bd390b0e87eaf50d3b
AsadBaig <92505464+AsadBaig-DEV@users.noreply.github.com>
2022-12-25 16:11:41 +0530

    API transformation for Current Weather

--- weather.js
+++ weather.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-
-// https://api.open-meteo.com/v1/forecast?latitude=19.08&longitude=72.89&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FChicago
-
 export function getWeather(lat, lon, timezone) {
   return axios.get(
     "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&precipitation_unit=inch&timeformat=unixtime&",
@@ -12,11 +9,40 @@ export function getWeather(lat, lon, timezone) {
         timezone,
       },
     }
-  ).then(({data}) {
+  ).then(({data}) => {
     return {
       current: parseCurrentWeather(data),
-      daily: parseDailyWeather(data),
-      hourly: parseHourlyWeather(data)
+      //daily: parseDailyWeather(data),
+      // hourly: parseHourlyWeather(data)
     }
   })
 }
+
+function parseCurrentWeather({ current_weather, daily }) {
+  const {
+    temperature: currentTemp,
+    windspeed: windSpeed,
+    weathercode: iconCode,
+  } = current_weather
+
+  const {
+    temperature_2m_max : [maxTemp],
+    temperature_2m_min : [minTemp],
+    apparent_temperature_max : [maxFeelsLike],
+    apparent_temperature_min : [minFeelsLike],
+    precipitation_sum : [precip],
+  } = daily
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  }
+}
+
+
